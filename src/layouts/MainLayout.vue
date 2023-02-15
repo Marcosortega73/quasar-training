2cd7053c056d706bf34c73391b0443eb90340a03
Marcos Ortega <mortega@na-sa.com.ar>
2023-02-15 11:37:16 -0300

    primer stash

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <NavBar />
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-dialog-wrapper>
+      <DialogLogin />
+    </q-dialog-wrapper>
+  </q-layout>
+</template>
+
+<script setup>
+import NavBar from "./components/NavBar.vue";
+import DialogLogin from "../components/DialogLogin.vue";
+import { defineComponent, ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+</script>
